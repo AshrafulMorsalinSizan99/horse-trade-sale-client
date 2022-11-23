9debd90d647310a22714e1d3fb63df7633d7641a
AshrafulMorsalinSizan99 <jucse28.377@gmail.com>
2022-11-24 00:48:49 +0600

    footer added

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -2,9 +2,28 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div>
+        <footer className="footer p-10 bg-neutral text-neutral-content">
+            <div>
+                <span className="footer-title">Services</span>
+                <a className="link link-hover" href='/'>Selling</a>
+                <a className="link link-hover" href='/'>Refunding</a>
+                <a className="link link-hover" href='/'>Marketing</a>
+                <a className="link link-hover" href='/'>Advertisement</a>
+            </div>
+            <div>
+                <span className="footer-title">Horse Trade Sale</span>
+                <a className="link link-hover" href='/'>About us</a>
+                <a className="link link-hover" href='/'>Contact</a>
+                <a className="link link-hover" href='/'>Jobs</a>
 
-        </div>
+            </div>
+            <div>
+                <span className="footer-title">Legal</span>
+                <a className="link link-hover" href='/'>Terms of use</a>
+                <a className="link link-hover" href='/'>Privacy policy</a>
+                <a className="link link-hover" href='/'>Cookie policy</a>
+            </div>
+        </footer>
     );
 };
 
