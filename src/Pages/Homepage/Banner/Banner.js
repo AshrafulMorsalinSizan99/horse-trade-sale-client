3aa17ed8a55d88d951934e9adb29e6ae0ff7ee03
AshrafulMorsalinSizan99 <jucse28.377@gmail.com>
2022-11-24 09:01:30 +0600

    extra section added

--- src/Pages/Homepage/Banner/Banner.js
+++ src/Pages/Homepage/Banner/Banner.js
@@ -1,18 +1,19 @@
 import React from 'react';
-import banner from '../../../assets/banner.png';
+
 
 const Banner = () => {
     return (
-        <div className="hero">
-            <div className="hero-content flex-col lg:flex-row-reverse mt-6">
-                <img src={banner} alt='' className=" rounded-lg w-1/2 shadow-2xl" />
-                <div>
-                    <h1 className="text-5xl font-bold">Resale Used Mobiles!</h1>
-                    <p className="py-6">In our websites, we are selling used mobiles which is in fresh conditions to use. You can get used phones of any company you prefer. Do visit us and crack your desired mobiles. Hoping to give you the best services. </p>
+        <div className="hero min-h-screen mb-5 mt-5" style={{ backgroundImage: `url("https://fscl01.fonpit.de/userfiles/6727621/image/2nd_YEAR/best_smartphones/AndroidPIT-best-smartphones-3-a-w1400h1050.jpg")` }}>
+            <div className="hero-overlay bg-opacity-60"></div>
+            <div className="hero-content text-center text-neutral-content">
+                <div className="max-w-md">
+                    <h1 className="mb-5 text-5xl font-bold">Resale Used Mobiles</h1>
+                    <p className="mb-5">We provide fresh used phone with reasonable price.Visit us and get to know about us more.You won't be disappointed</p>
                     <button className="btn btn-accent">Get Started</button>
                 </div>
             </div>
         </div>
+
     );
 };
 
---
+++ src/Pages/Homepage/ExtraSection/ExtraSection.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import banner from '../../../assets/banner.png'
+
+const ExtraSection = () => {
+    return (
+        <div className="hero">
+            <div className="hero-content flex-col lg:flex-row-reverse mt-6">
+                <img src={banner} alt='' className=" rounded-lg w-1/2 shadow-2xl" />
+                <div>
+                    <h1 className="text-5xl font-bold">Wanna Buy Used Mobiles??</h1>
+                    <p className="py-6">In our websites, we are selling used mobiles which is in fresh conditions to use. You can get used phones of any company you prefer. Do visit us and crack your desired mobiles. Hoping to give you the best services. </p>
+                    <button className="btn btn-accent">Get Started</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ExtraSection;
